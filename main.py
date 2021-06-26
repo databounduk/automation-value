84d6770c454ba6309e84f405f83c0fe1b6e48764
lchrimes <lchrimes@databound.co.uk>
2021-06-26 17:04:18 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,75 @@
+from os import read
+import streamlit as st
+import pandas as pd
+import pickle
+from jinja2 import Template
+
+st.set_page_config(page_title="Automation Potential")
+
+st.title("Automation Potential")
+
+
+# Reading Static Data
+with open("assets/html/header.html", "r") as f:
+    st.markdown(f.read(), unsafe_allow_html=True)
+
+
+def load_data(filename):
+    try:
+        with open(f"{filename}.pkl", "rb") as f:
+            data = pickle.load(f)
+    except Exception as E:
+        print(f"error on load : {E}")
+        return
+    return data
+
+
+job_titles: dict = load_data("job_title")
+
+job_selected = st.selectbox(
+    "Position Held By Employee Doing Task?", list(job_titles.keys())
+)
+
+hourly_sal = format(
+    (int(job_titles[job_selected].replace("£", "").replace(",", "")) / 52) / 37, ".2f"
+)
+
+st.text(f"Average Yearly Salary : {job_titles[job_selected]}")
+st.text(f"Average Hourly Rate : £{hourly_sal}")
+
+weekly_time_spent = st.number_input(
+    "How many hours does this task take per week for all employees?",
+    min_value=1,
+    value=5,
+)
+
+savings = float(hourly_sal) * int(weekly_time_spent)
+average_revenue_yearly = 118000
+average_revenue_hourly = (average_revenue_yearly / 52) / 37
+
+weekly_savings = format(float(hourly_sal) * int(weekly_time_spent), ".2f")
+yearly_savings = format(float(hourly_sal) * int(weekly_time_spent) * 52, ".2f")
+
+st.text(f"Yearly Hours Saved : {int(weekly_time_spent)*52}")
+st.text(f"Weekly Cost of Task : £{weekly_savings}")
+st.text(f"Yearly Cost of Task : £{yearly_savings}")
+
+potential_revenue = format(average_revenue_hourly * weekly_time_spent * 52, ".2f")
+st.text(f"Potential Revenue: £{potential_revenue}")
+total_year_savings = format(float(potential_revenue) + float(yearly_savings), ".2f")
+# st.text(f"Total Potential Savings : £{total_year_savings}")
+
+
+pd.read_csv(
+    "https://www.ethnicity-facts-figures.service.gov.uk/work-pay-and-benefits/employment/employment-by-occupation/latest/downloads/employment-by-occupation.csv"
+)
+
+with open("assets/html/savings.html", "r") as f:
+    t = Template(f.read())
+    variables = {
+        "hours": int(weekly_time_spent) * 52,
+        "revenue_potential": f"£{total_year_savings}",
+    }
+    st.markdown(
+        t.render(variables), unsafe_allow_html=True,
+    )
---
+++ readme.md
@@ -0,0 +1,5 @@
+Resource Link:
+
+
+- Salaries Data : https://inews.co.uk/inews-lifestyle/work/this-is-the-average-uk-salary-for-270-jobs-in-2019-319070
+- Average Employee Generation 👍 https://www.accountsandlegal.co.uk/small-business-advice/average-uk-employee-generates-118-000-of-revenue-per-year
